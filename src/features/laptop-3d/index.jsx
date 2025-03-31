67329612af30d4d2a5e55fdd4980b0e9a43951d2
Kamil Aghayev <kamilizundef@gmail.com>
2025-03-31 22:11:27 +0400

    `Removed MacBook 3D model and related assets, updated laptop-3D component to use a new model`

--- src/features/laptop-3d/index.jsx
+++ src/features/laptop-3d/index.jsx
@@ -1,9 +1,11 @@
 'use client'
 
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
+import { ContactShadows, Environment, OrbitControls, PerspectiveCamera } from '@react-three/drei';
 import { MacBook } from './MacBook';
 import { Vector3 } from 'three';
+import { degToRad } from 'three/src/math/MathUtils';
+import { Suspense } from 'react';
 const TextOnScreen = () => {
 	return (
 		<h1 style={{ fontSize: "40px", color: "black" }}>Salam</h1>
@@ -12,15 +14,18 @@ const TextOnScreen = () => {
 
 export const Laptop3d = () => {
 	return (
-		<Canvas>
-			<OrbitControls enableDamping={true} minDistance={9} maxDistance={15} target={[0, 1,0]}/>
-			<gridHelper args={[20, 20, 20]} />
-			<axesHelper args={[60]} />
-			<MacBook ><TextOnScreen/></MacBook>
-
-			<ambientLight intensity={0.1} />
-
-			<PerspectiveCamera makeDefault position={[4, 5, 10]} fov={75}/>
+		<Canvas camera={{ position: [5, 12, 15], fov: 55 }}>
+			<pointLight position={[10, 10, 10]} intensity={1.5} />
+			<axesHelper />
+			<gridHelper/>
+			<Suspense fallback={null}>
+				<group rotation={[0, 0, 0]} position={[0, 1, 0]}>
+					<MacBook />
+				</group>
+				<Environment preset="city" />
+			</Suspense>
+			<ContactShadows position={[0, -3, 0]} scale={20} blur={4} far={4.5} />
+			<OrbitControls enablePan={false} enableZoom={true} minDistance={15} maxDistance={18} minPolarAngle={Math.PI / 2.2} maxPolarAngle={Math.PI / 2.2} />
 		</Canvas>
 	)
 }
