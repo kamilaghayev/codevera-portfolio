90e7e72770251d68a8d161f532f456c642c2fcc5
hazratahmdzd <B062192021@asoiu.edu.az>
2025-04-01 12:56:15 +0400

    added projects for portfolio section and page

--- src/app-pages/home/_sections/our-projects.jsx
+++ src/app-pages/home/_sections/our-projects.jsx
@@ -2,17 +2,54 @@ import React from 'react'
 import { ProjectCard, MoreButton } from '@/components'
 
 export const OurProjects = () => {
+
+  const projects = [
+    {
+      name: "Turizm.az",
+      src: "/png/turizm-az.png",
+      link: "https://www.turizm.az/",
+      types: ["Ecommerce", "Corporative"],
+    },
+    {
+      name: "Muhteşem 3lü",
+      src: "/png/muhtesem-3lu.png",
+      link: "https://www.muhtesem3lu.az/",
+      types: ["Business", "Education"],
+    },
+    {
+      name: "Luminous",
+      src: "/png/luminous.png",
+      link: "https://www.luminoustours.az/",
+      types: ["Ecommerce", "Corporative"],
+    },
+    {
+      name: "Mastercard Promo",
+      src: "/png/mastercard-promo.png",
+      link: "https://mastercard-promo.by/",
+      types: ["Business", "Corporative"],
+    },
+    {
+      name: "Hunter-Sale",
+      src: "/png/hunter-sale.png",
+      link: "https://www.hunter-sale.com/",
+      types: ["Ecommerce", "Corporative", "Business"],
+    },
+    {
+      name: "Rahatviza",
+      src: "/png/rahat-viza.png",
+      link: "https://rahatviza.az/",
+      types: ["Corporative", "Business"],
+    },
+  ]
+
   return (
     <section aria-labelledby='our-projects' id='portfolio'>
         <div className='container-center flex flex-col gap-6 sm:gap-10 lg:gap-14'>
             <h2 id='advantages' className='text-3xl sm:text-4xl lg:text-6xl font-semibold'>Our projects</h2>
             <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10'>
-                <ProjectCard />
-                <ProjectCard />
-                <ProjectCard />
-                <ProjectCard />
-                <ProjectCard />
-                <ProjectCard />
+                {projects.map((project, index) => (
+                  <ProjectCard key={index} name={project.name} src={project.src} link={project.link} types={project.types} />
+                ))}
             </div>
             <div className='flex justify-center'>
                 <MoreButton path='/portfolio' />
--- src/components/cards/project-card.jsx
+++ src/components/cards/project-card.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
-export const ProjectCard = () => {
+export const ProjectCard = ({ name = "CodeVera", src = "/project.png", link = "https://codevera.com", types = [ "Ecommerce", "Corporative" ] }) => {
   return (
     <div className='w-full flex flex-col gap-4 sm:gap-6'>
-        <div className='w-full aspect-video p-4 sm:p-5 lg:p-5 rounded-3xl bg-[#F5F5F5]'>
-            <div style={{backgroundImage: 'url(/project.png)'}} className='w-full h-full rounded-3xl bg-cover'></div>
-        </div>
+        <Link href={link} className='w-full aspect-video p-4 sm:p-5 lg:p-5 rounded-3xl bg-[#F5F5F5]'>
+            <Image src={src} alt={`${name} Screenshot`} width={300} height={300} className='w-full h-full object-contain rounded-3xl'/>
+        </Link>
         <div className='flex flex-col gap-2 sm:gap-4'>
-            <h3 className='text-xl sm:text-3xl lg:text-4xl font-semibold'>CodeVera</h3>
+            <h3 className='text-xl sm:text-3xl lg:text-4xl font-semibold'>{name}</h3>
             <div className="w-full flex gap-4 flex-nowrap overflow-x-scroll pb-3">
-                <div className="min-w-fit px-4 py-2 bg-[#E6E6E6] text-[#141414] rounded-full text-xs sm:text-sm lg:text-base font-medium">
-                    Ecommerce
-				</div>
-                <div className="min-w-fit px-4 py-2 bg-[#E6E6E6] text-[#141414] rounded-full text-xs sm:text-sm lg:text-base font-medium">
-                    Corporative
-				</div>
+                {types.map((type, index) => (
+                    <div key={index} className="min-w-fit px-4 py-2 bg-[#E6E6E6] text-[#141414] rounded-full text-xs sm:text-sm lg:text-base font-medium">
+                        {type}
+				    </div>
+                ))}
             </div>
         </div>
     </div>
