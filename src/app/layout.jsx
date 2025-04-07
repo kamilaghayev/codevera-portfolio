7a533dc11f8a51a5cc43a644067527cb5ee39b5e
Kamil Aghayev <kamilizundef@gmail.com>
2025-04-07 12:52:42 +0400

    `feat: add @vercel/analytics to package-lock.json and package.json, and integrate it into src/app/layout.jsx`

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,37 +1,38 @@
-import { Geist, Nunito_Sans } from "next/font/google";
-import Providers from './(initialize)/providers';
-import { Header, Footer } from '@/components';
+import { Footer, Header } from '@/components';
 import { metadataMain } from '@/config/seo.config';
+import { Geist, Nunito_Sans } from 'next/font/google';
+
 import '@ant-design/v5-patch-for-react-19';
-import "./globals.css";
+import { Analytics } from '@vercel/analytics/next';
+
 import { LaptopContainer } from './(initialize)/laptop-initialize';
+import Providers from './(initialize)/providers';
+import './globals.css';
 
 const nunito = Nunito_Sans({
-  variable: "--font-nunito",
-  subsets: ["latin"],
-  weight: ['200', '300', '400', '700', '900']
+    variable: '--font-nunito',
+    subsets: ['latin'],
+    weight: ['200', '300', '400', '700', '900'],
 });
 
 export const metadata = metadataMain;
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <body
-        className={`${nunito.variable} antialiased overflow-x-hidden`}
-      >
-        <Providers>
-          <Header />
-          {children}
-          <Footer />
-        </Providers>
-        <div className='overflow-hidden'>
-          <LaptopContainer/>
-
-        </div>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <meta charSet="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <body className={`${nunito.variable} antialiased overflow-x-hidden`}>
+                <Providers>
+                    <Header />
+                    {children}
+                    <Footer />
+                </Providers>
+                <div className="overflow-hidden">
+                    <LaptopContainer />
+                </div>
+                <Analytics />
+            </body>
+        </html>
+    );
 }
