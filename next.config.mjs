3d08bb69ad8e238091042f4273e1d4316d6a8297
elgun030 <elgunng@code.edu.az>
2025-06-05 22:46:16 +0400

    fixed links

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     transpilePackages: ['three'],
+    images: {
+        domains: ['cavid.s3.eu-north-1.amazonaws.com'],
+    },
 };
 
 export default nextConfig;
